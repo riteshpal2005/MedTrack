c94662c51d85be6837a5ead187471a2ae29498f1
Ritesh Pal <ritesh.dm.211@gmail.com>
2026-07-20 18:30:52 +0530

    chore(test): install and configure react-native-testing-library

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: '@react-native/jest-preset',
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native(-community)?|@nozbe/watermelondb)/',
+  ],
 };
