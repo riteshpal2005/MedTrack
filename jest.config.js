4275a45adb7c8f073fa7cc0cd4cc82132758ee17
Ritesh Pal <ritesh.dm.211@gmail.com>
2026-07-20 18:43:07 +0530

    fix(test): update jest preset to @react-native/jest-preset and fix db mock typo

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'react-native',
+  preset: '@react-native/jest-preset', // <-- CHANGE THIS LINE
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native(-community)?|@nozbe/watermelondb)/',
