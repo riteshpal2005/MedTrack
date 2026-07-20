da647d7bafad212d98f7534cd39bd40bf764b7a5
Ritesh Pal <ritesh.dm.211@gmail.com>
2026-07-20 16:57:48 +0530

    chore: completed watermelondb infrastructure setup

---
+++ src/database/index.ts
@@ -0,0 +1,24 @@
+import { Database } from '@nozbe/watermelondb';
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
+
+import schema from './schema';
+import Profile from './models/Profile';
+import Medicine from './models/Medicine';
+import HistoryLog from './models/HistoryLogs';
+
+const adapter = new SQLiteAdapter({
+  schema,
+  jsi: true,
+  onSetUpError: error => {
+    console.error('Databse failed to load: ', error);
+  }
+});
+
+export const database = new Database({
+  adapter,
+  modelClasses: [
+    Profile,
+    Medicine,
+    HistoryLog,
+  ],
+});
