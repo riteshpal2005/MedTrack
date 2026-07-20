6060fb4823f5d378910d87c399610613a2afea9d
Ritesh Pal <ritesh.dm.211@gmail.com>
2026-07-20 18:43:14 +0530

    fix(babel): update decorators plugin config to use version legacy for jest compatibility

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
   plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }]
+    ['@babel/plugin-proposal-decorators', { version: 'legacy' }]
   ]
 };
