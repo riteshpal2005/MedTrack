024afba0a6bfb3d602197c5ff8d11a98adba07da
Ritesh Pal <ritesh.dm.211@gmail.com>
2026-07-20 18:37:12 +0530

    test: add unit test for profile database controller

---
+++ __tests__/profile.test.ts
@@ -0,0 +1,27 @@
+import { createProfile } from "../src/database/helpers/profile";
+
+jest.mock('../src/database', () => {
+  return {
+    databse: {
+      write: jest.fn(async (callback) => callback()),
+      collections: {
+        get: jest.fn(() => ({
+          create: jest.fn((callback) => {
+            const profile: any = {};
+            callback(profile);
+            return profile;
+          }),
+        })),
+      },
+    },
+  };
+});
+
+describe('Profile Business Logic', () => {
+  it('should successfully format and create a new profile with the given name', async () => {
+    const profile = await createProfile('Test User');
+    expect(profile.name).toBe('Test User');
+
+    expect(profile.createdAt).toBeDefined();
+  });
+});
