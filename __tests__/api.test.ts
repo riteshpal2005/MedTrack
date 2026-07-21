e9a5f64cd9d5c51e82e7c80522103c688534f5f6
Ritesh Pal <ritesh.dm.211@gmail.com>
2026-07-21 10:57:10 +0530

    test(sync): implement exhaustive security tests for jwt authentication and zod validation

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,6 +1,15 @@
 import { pushProfileToServer } from "../src/api/sync";
+import { api } from "../src/api/axios";
 
 describe('API Sync Logic', () => {
+
+  beforeAll( async () => {
+    const res = await api.post('/auth/login', { apiKey: 'MEDTRACK_SECURE_SYNC_123' });
+    const token = res.data.token;
+
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  });
+
   it('should succesfully push a new profile to the local Postgres backend', async () => {
     const randomId = `test_profile_${Math.floor(Math.random() * 10000)}`;
 
@@ -12,5 +21,19 @@ describe('API Sync Logic', () => {
 
     expect(result.name).toBe('Jest PC Test User');
     expect(result.id).toBe(randomId);
+  });
+
+  it('should instantly block a malicious payload (Zod Security Firewall)', async () => {
+    expect.assertions(1);
+
+    try {
+      await pushProfileToServer({
+        id: 'hacker_123',
+        name: '',
+        createdAt: Date.now()
+      });
+    } catch (error: any) {
+      expect(error.response.status).toBe(400);
+    }
   });
 });
